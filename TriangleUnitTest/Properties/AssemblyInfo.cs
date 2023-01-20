b527caf9e6696d20c6d4b029d35bf1d8a50f530e
chickennuggies <116859483+KIDDOs1412@users.noreply.github.com>
2023-01-20 12:21:33 +0800

    Add files via upload

---
+++ TriangleUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriangleUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriangleUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6aceb1c0-6d5a-4132-91e1-777356311cb1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TriangleUnitTest/UnitTest.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Trianglelibrary;
+
+namespace TriangleUnitTest
+{
+    [TestClass]
+    public class UnitTestBySide
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+
+            Assert.AreEqual(atriangle.SideA, 0);
+            Assert.AreEqual(atriangle.SideB, 0);
+            Assert.AreEqual(atriangle.SideC, 0);
+
+        }
+
+        [TestMethod]
+        public void TestProperties()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 2;
+            atriangle.SideB = 3;
+            atriangle.SideC = 4;
+
+            Assert.AreEqual(atriangle.SideA, 2);
+            Assert.AreEqual(atriangle.SideB, 3);
+            Assert.AreEqual(atriangle.SideC, 4);
+
+        }
+
+        [TestMethod]
+        public void TestArea()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 3;
+            atriangle.SideB = 4;
+            atriangle.SideC = 5;
+
+            Assert.AreEqual(atriangle.Area(), 6);
+        }
+
+
+        [TestMethod]
+        public void TestPerimeter()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 3;
+            atriangle.SideB = 4;
+            atriangle.SideC = 5;
+
+            Assert.AreEqual(atriangle.Perimeter(), 12);
+        }
+
+
+        [TestMethod]
+        public void TestAngle()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 3;
+            atriangle.SideB = 4;
+            atriangle.SideC = 5;
+
+            Assert.AreEqual(atriangle.AngleAB(), 90);
+            Assert.AreEqual(atriangle.AngleAC(), 53);
+            Assert.AreEqual(atriangle.AngleBC(), 37);
+
+        }
+
+        [TestMethod]
+        public void TestHalfPerimeter()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 3;
+            atriangle.SideB = 4;
+            atriangle.SideC = 5;
+
+            Assert.AreEqual(atriangle.S(), 6);
+
+        }
+    }
+
+    [TestClass]
+    public class UnitTestByPoint
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            TriangleByPoint atriangle = new TriangleByPoint();
+
+            Assert.AreEqual(atriangle.SideA(), 0);
+            Assert.AreEqual(atriangle.SideB(), 0);
+            Assert.AreEqual(atriangle.SideC(), 0);
+
+        }
+
+        [TestMethod]
+        public void TestProperties()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 2;
+            atriangle.SideB = 3;
+            atriangle.SideC = 4;
+
+            Assert.AreEqual(atriangle.SideA, 2);
+            Assert.AreEqual(atriangle.SideB, 3);
+            Assert.AreEqual(atriangle.SideC, 4);
+
+        }
+
+        [TestMethod]
+        public void TestArea()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 3;
+            atriangle.SideB = 4;
+            atriangle.SideC = 5;
+
+            Assert.AreEqual(atriangle.Area(), 6);
+        }
+
+
+        [TestMethod]
+        public void TestPerimeter()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 3;
+            atriangle.SideB = 4;
+            atriangle.SideC = 5;
+
+            Assert.AreEqual(atriangle.Perimeter(), 12);
+        }
+
+
+        [TestMethod]
+        public void TestAngle()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 3;
+            atriangle.SideB = 4;
+            atriangle.SideC = 5;
+
+            Assert.AreEqual(atriangle.AngleAB(), 90);
+            Assert.AreEqual(atriangle.AngleAC(), 53);
+            Assert.AreEqual(atriangle.AngleBC(), 37);
+
+        }
+
+        [TestMethod]
+        public void TestHalfPerimeter()
+        {
+            TriangleBySide atriangle = new TriangleBySide();
+            atriangle.SideA = 3;
+            atriangle.SideB = 4;
+            atriangle.SideC = 5;
+
+            Assert.AreEqual(atriangle.S(), 6);
+
+        }
+    }
+}
